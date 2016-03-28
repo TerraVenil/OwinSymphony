78f692bee0a0c07ea5d755704e942d70f19e0791
Oleksiy Borodenko <oboroden@softserveinc.com>
2016-03-28 14:17:06 +0300

    Add empty Login Controller/View.

---
+++ src/IdentityMelody/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace IdentityMelody.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult Login()
+        {
+            return View();
+        }
+    }
+}
