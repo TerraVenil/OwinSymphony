e4ad6d59854c10cf34e388675990e724578b117f
Oleksiy Borodenko <oboroden@softserveinc.com>
2016-03-28 14:56:14 +0300

    Implementation form authentication based on cookie.

--- src/IdentityMelody/Controllers/AccountController.cs
+++ src/IdentityMelody/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Web.Mvc;
+using System.Web.Security;
+using IdentityMelody.Infrastructure;
+using IdentityMelody.Models;
+using Microsoft.AspNet.Identity;
 
 namespace IdentityMelody.Controllers
 {
@@ -7,6 +11,29 @@ namespace IdentityMelody.Controllers
         public ViewResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            using (var userManager = IdentityMelodyUserManager.Create())
+            {
+                var user = userManager.Find(model.UserName, model.Password);
+
+                if (user != null)
+                {
+                    FormsAuthentication.SignOut();
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+
+                    return Redirect(returnUrl ?? Url.Action("Index", "Home"));
+                }
+
+                ModelState.AddModelError("", "Incorrect username or password");
+                return View();
+            }
         }
     }
 }
