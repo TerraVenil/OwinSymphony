076abb781cce91f002c9b451cc34980a22bf2a8e
Oleksiy Borodenko <oboroden@softserveinc.com>
2016-03-28 14:30:37 +0300

    Add ability to create users.

--- src/IdentityMelody/Controllers/HomeController.cs
+++ src/IdentityMelody/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Mvc;
 using IdentityMelody.Infrastructure;
+using IdentityMelody.Models;
+using Microsoft.AspNet.Identity;
 
 namespace IdentityMelody.Controllers
 {
@@ -10,6 +12,40 @@ namespace IdentityMelody.Controllers
             using (var userManager = IdentityMelodyUserManager.Create())
             {
                 return View(userManager.Users);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateMusicUserModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = new MusicUser { UserName = model.Name, Email = model.Email };
+            using (var userManager = IdentityMelodyUserManager.Create())
+            {
+                var result = userManager.Create(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                AddErrorsFromResult(result);
+            }
+
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
             }
         }
     }
