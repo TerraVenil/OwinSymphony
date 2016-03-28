f68e1193b477a6b4a11575d89a94016b3f8be3a6
Oleksiy Borodenko <oboroden@softserveinc.com>
2016-03-28 12:56:03 +0300

    Add empty Home Controller/View and base layout.

---
+++ src/IdentityMelody/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace IdentityMelody.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var data = new Dictionary<string, object> { { "Placeholder", "Placeholder" } };
+            return View(data);
+        }
+    }
+}
---
+++ src/IdentityMelody/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentityMelody.Global" Language="C#" %>
